1f54cc624b1ebe04bf2b3a50dd3bc26446959e62
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-07-03 08:10:46 -0300

    refac: retira console.log

--- src/Controller/ConsultaController.ts
+++ src/Controller/ConsultaController.ts
@@ -18,33 +18,21 @@ export class ConsultaController {
 
       const medico = await medicoRepository.findOne({ where: { id: medicoId } });
       if (!medico) {
-        console.log(`Médico com ID: ${medicoId} não encontrado.`);
         return res.status(404).json({ error: "Médico não encontrado." });
       }
-
-      console.log(`Buscando paciente com ID: ${pacienteId}`);
       const paciente = await pacienteRepository.findOne({ where: { id: pacienteId } });
       if (!paciente) {
-        console.log(`Paciente com ID: ${pacienteId} não encontrado.`);
         return res.status(404).json({ error: "Paciente não encontrado." });
       }
-
-      console.log(`Verificando se a data é um feriado: ${data}`);
       if (isHoliday(new Date(data))) {
-        console.log(`A data ${data} é um feriado.`);
         return res.status(400).json({ error: "Não é permitido cadastrar consultas em feriados." });
       }
-
-      console.log(`Verificando horário de trabalho do médico`);
       if (!validateWorkingHours(medico, new Date(data), horario, duracao)) {
-        console.log(`Horário fora do expediente do médico.`);
         return res.status(400).json({ error: "Horário fora do expediente do médico." });
       }
 
       const horarioFinal = new Date(`${data}T${horario}`);
       horarioFinal.setMinutes(horarioFinal.getMinutes() + duracao);
-
-      console.log(`Verificando conflitos de horário`);
       const conflitos = await consultaRepository.find({
         where: {
           medico,
@@ -54,11 +42,9 @@ export class ConsultaController {
       });
 
       if (conflitos.length > 0) {
-        console.log(`Conflito de horário com outra consulta.`);
         return res.status(400).json({ error: "Conflito de horário com outra consulta." });
       }
 
-      console.log(`Criando consulta`);
       const consulta = consultaRepository.create({
         paciente,
         medico,
@@ -66,10 +52,7 @@ export class ConsultaController {
         horario,
         duracao
       });
-
-      console.log(`Salvando consulta no banco de dados`);
       await consultaRepository.save(consulta);
-      console.log(`Consulta criada com sucesso: ${JSON.stringify(consulta)}`);
       res.status(201).json(consulta);
     } catch (error: unknown) {
       if (error instanceof Error) {
